1cfb339699c356d16b44b56c0112326b4cc7b3d0
yashmeetop <157510016+yashmeetop@users.noreply.github.com>
2025-06-20 17:07:35 +0530

    performance_boost_and_React_Router_Refresh_error_fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -25,7 +25,7 @@ function App() {
     }
 
     return (
-        <Router>
+        <Router basename={'/'}>
             {/* NEW: ToastContainer for global notifications */}
             <ToastContainer
                 position="top-right"
